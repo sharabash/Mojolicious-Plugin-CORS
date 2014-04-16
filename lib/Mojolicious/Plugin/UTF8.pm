258222b869879c0826bc2a52d86caf22970a9f86
Nour Sharabash <nour.sharabash@gmail.com>
2014-04-16 17:53:59 -0500

    init

---
+++ lib/Mojolicious/Plugin/UTF8.pm
@@ -0,0 +1,30 @@
+package Mojolicious::Plugin::CORS;
+use Mojo::Base 'Mojolicious::Plugin';
+
+sub register {
+    my ( $self, $app, $opts ) = @_;
+    CORS: {
+        $app->hook( before_dispatch => sub {
+            my $c = shift;
+            $c->res->headers->header( 'Access-Control-Allow-Origin' => '*' );
+            $c->res->headers->header( 'Access-Control-Allow-Methods' => 'POST, GET, PUT, DELETE, OPTIONS' );
+            $c->res->headers->header( 'Access-Control-Max-Age' => 3600 );
+            $c->res->headers->header( 'Access-Control-Allow-Headers' => 'X-Requested-With' );
+        } );
+    };
+}
+
+1;
+
+=head1 SYNOPSIS
+
+    package App;
+    use Mojo::Base 'Mojolicious';
+
+    sub startup {
+        my $self = shift;
+
+        $self->plugin( 'Mojolicious::Plugin::CORS' );
+    }
+
+=cut
