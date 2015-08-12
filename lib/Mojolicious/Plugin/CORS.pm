fbf1fb97e0caa7764b8452fd92164f3aff56951a
Fernando Correa <fernandocorrea@gmail.com>
2015-08-12 03:44:23 +0000

    Fix config

--- lib/Mojolicious/Plugin/CORS.pm
+++ lib/Mojolicious/Plugin/CORS.pm
@@ -1,21 +1,22 @@
 package Mojolicious::Plugin::CORS;
 use Mojo::Base 'Mojolicious::Plugin';
-
-my %defaults = (
-    allow_origin	=> [qw/*/],
-    allow_methods	=> [qw/GET PUT POST DELETE OPTIONS/],
-    max_age		=> [qw/600/],
-    allow_headers	=> [qw/Content-Type Authorization X-Requested-With/],
-);
+use Clone qw/clone/;
 
 sub register {
-    my %heders = %defaults;
     my ( $self, $app, $opts ) = @_;
+    my %defaults = (
+        allow_origin	=> [qw/*/],
+        allow_methods	=> [qw/GET PUT POST DELETE OPTIONS/],
+        max_age		=> [qw/3600/],
+        allow_headers	=> [qw/Content-Type Authorization X-Requested-With/],
+    );
 
-    for my $key(keys %defaults) {
-        if(substr($key, 0, 1) eq "+") {
-            substr($key, 0, 1) = "";
-            push $headers{$key}, @{ ref $opts->{$key} eq "ARRAY" ? $opts->{$key} : [$opts->{$key}] };
+    my %headers = %{ clone \%defaults };
+
+    for my $key(keys %$opts) {
+	if(substr($key, 0, 1) eq "+") {
+	    my $name = substr($key, 1);
+            push @{ $headers{$name} }, @{ ref $opts->{$key} eq "ARRAY" ? $opts->{$key} : [$opts->{$key}] };
         } else {
              $headers{$key} = ref $opts->{$key} eq "ARRAY" ? $opts->{$key} : [$opts->{$key}];
         }
