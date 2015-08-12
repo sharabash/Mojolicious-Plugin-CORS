b157aad5bffee2b180bff6e940da19daa906d400
FCO <fernandocorrea@gmail.com>
2015-08-11 23:31:59 -0300

    Make it configurable

--- lib/Mojolicious/Plugin/CORS.pm
+++ lib/Mojolicious/Plugin/CORS.pm
@@ -1,17 +1,39 @@
 package Mojolicious::Plugin::CORS;
 use Mojo::Base 'Mojolicious::Plugin';
 
+my %defaults = (
+    allow_origin	=> [qw/*/],
+    allow_methods	=> [qw/GET PUT POST DELETE OPTIONS/],
+    max_age		=> [qw/600/],
+    allow_headers	=> [qw/Content-Type Authorization X-Requested-With/],
+);
+
 sub register {
+    my %heders = %defaults;
     my ( $self, $app, $opts ) = @_;
+
+    for my $key(keys %defaults) {
+        if(substr($key, 0, 1) eq "+") {
+            substr($key, 0, 1) = "";
+            push $headers{$key}, @{ ref $opts->{$key} eq "ARRAY" ? $opts->{$key} : [$opts->{$key}] };
+        } else {
+             $headers{$key} = ref $opts->{$key} eq "ARRAY" ? $opts->{$key} : [$opts->{$key}];
+        }
+    }
+
     CORS: {
         $app->hook( before_dispatch => sub {
             my $c = shift;
-            $c->res->headers->header( 'Access-Control-Allow-Origin' => '*' );
-            $c->res->headers->header( 'Access-Control-Allow-Methods' => 'GET, PUT, POST, DELETE, OPTIONS' );
-            $c->res->headers->header( 'Access-Control-Max-Age' => 3600 );
-            $c->res->headers->header( 'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With' );
+	    for my $header(keys %headers) {
+                $c->res->headers->header( translate_name($header) => join ", ", @{ $headers{$header} } );
+	    }
         } );
     };
+}
+
+sub translate_name {
+	my $name = shift;
+	join "-", "Access-Control", map {ucfirst} split /_/, $name
 }
 
 1;
